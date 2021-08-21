07b30cd2837867e4f74db9fbdd870cdab5633a05
Praveenram Balachandran <praveenrambalu@gmail.com>
2021-08-21 11:13:19 +0530

    db migrated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('user_type')->default('user')->nullable();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phoneno')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
