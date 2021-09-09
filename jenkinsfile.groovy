b8522201196d1630ba4e70f81a8c9414e0bbee6d
doluz <sidhu.n.india@gmail.com>
2021-09-09 21:13:09 +0530

    added jenkinsfile for testing ec2 prod deployment

---
+++ jenkinsfile.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    stages {
+      def remote = [:]
+remote.name = "node"
+remote.host = "node.abc.com"
+remote.allowAnyHosts = true
+
+node {
+    withCredentials([usernamePassword(credentialsId: 'hiyamee-tracker-prod', passwordVariable: 'password', usernameVariable: 'ubuntu')]) {
+        remote.user = userName
+        remote.password = password
+
+        stage("SSH Steps Rocks!") {
+            writeFile file: 'test.sh', text: 'ls'
+            sshCommand remote: remote, command: 'for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done'
+            sshScript remote: remote, script: 'test.sh'
+            sshPut remote: remote, from: 'test.sh', into: '.'
+            sshGet remote: remote, from: 'test.sh', into: 'test_new.sh', override: true
+            sshRemove remote: remote, path: 'test.sh'
+                 }
+             }
+        }
+    }
+}
