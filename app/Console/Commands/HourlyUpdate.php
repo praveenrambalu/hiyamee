582d4c20ba08135928a51211d4ce012a873cdb83
Praveenram Balachandran <praveenrambalu@gmail.com>
2021-09-09 15:43:57 +0530

    mail notification hourly update is done

---
+++ app/Console/Commands/HourlyUpdate.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Candidate;
+use App\Models\User;
+use App\Notifications\InterviewReminder;
+use Illuminate\Console\Command;
+
+class HourlyUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hour:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send a reminder before 1 hour Interview';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $today = date("Y-m-d", strtotime('today'));
+        $now = date("H:i", strtotime('+1 hour'));
+        $candidates = Candidate::where('interview_outcome', 'Ready')
+            ->where('interview_date', $today)
+            ->where('interview_completed_at', NULL)
+            ->where('interview_time', '<=', $now)
+            ->distinct()->get(['allocated_to']);
+        if (count($candidates) > 0) {
+            foreach ($candidates as $candidate) {
+                if ($rec = User::find($candidate->allocated_to)) {
+                    $rec->notify(new InterviewReminder($rec->name));
+                }
+            }
+        }
+        $this->info('Hourly Update has been send successfully');
+    }
+}
---
+++ app/Notifications/InterviewReminder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class InterviewReminder extends Notification
+{
+    use Queueable;
+    public $name;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Interview Reminder')
+            ->greeting('Dear  ' . $this->name)
+            ->line('This message is to remind you have an interview within One hour')
+            ->line('Please check on your dashboard')
+            ->action('Check Here', url('/dashboard'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
